a364c84f3688842f46c18ece1cd3545f9e88e785
Apda-coder <apdlahany@gmail.com>
2025-08-15 20:39:54 +0300

    رجوع للنسخة القديمة من الكوميت b822bbd

---
+++ supabase-config.js
@@ -0,0 +1,5 @@
+    // تهيئة العميل
+    const supabaseClient = supabase.createClient(
+      "https://zefsmckaihzfiqqbdake.supabase.co",
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InplZnNtY2thaWh6ZmlxcWJkYWtlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQyMzUzNTgsImV4cCI6MjA2OTgxMTM1OH0.vktk2VkEPtMclb6jb_pFa1DbrqWX9SOZRsBR577o5mc"
+    );
